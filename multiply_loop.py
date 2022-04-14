50d64fd07f0aa715b66b038c12486c7231cf8bfe
Yiyun Qin <yiyun.qin@mths.ca>
2022-04-14 02:10:02 +0000

    multiply loop program

---
+++ multiply_loop.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Created by Yiyun Qin
+# Created in April 2022
+# This is the math program, multiplying number from 1 to n
+
+
+def main():
+    # This function multiplys number from 1 to number the user enters
+
+    # input
+    multiply_string = input("Enter a positive integer: ")
+
+    # process & output
+    loop_counter = 0
+    product = 1
+    print("")
+    try:
+        multiply_integer = int(multiply_string)
+        if multiply_integer > 0:
+            while loop_counter < multiply_integer:
+                loop_counter = loop_counter + 1
+                product = product * loop_counter
+            print("{0}! = {1}".format(multiply_string, product))
+        elif multiply_integer == 0:
+            print("0! = 1")
+        else:
+            print("You did not enter a positive number.")
+    except Exception:
+        print("Invalid number!")
+    finally:
+        print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
